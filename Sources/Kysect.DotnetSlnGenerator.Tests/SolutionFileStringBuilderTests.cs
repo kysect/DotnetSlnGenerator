7f7160ffa66314e29e5b0015873e519a0f6a5fce
Fredi Kats <fredikats@outlook.com>
2023-12-26 00:19:12 +0100

    Add test for solution file building

---
+++ Sources/Kysect.DotnetSlnGenerator.Tests/SolutionFileStringBuilderTests.cs
@@ -0,0 +1,33 @@
+using FluentAssertions;
+using Kysect.DotnetSlnGenerator.Builders;
+
+namespace Kysect.DotnetSlnGenerator.Tests;
+
+public class SolutionFileStringBuilderTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Build_WithoutProject_ReturnExpectedString()
+    {
+        var solutionFileStringBuilder = new SolutionFileStringBuilder();
+        var expected = """
+                       Microsoft Visual Studio Solution File, Format Version 12.00
+                       # Visual Studio Version 17
+                       VisualStudioVersion = 17.9.34310.174
+                       MinimumVisualStudioVersion = 10.0.40219.1
+                       Global
+                       	GlobalSection(SolutionProperties) = preSolution
+                       		HideSolutionNode = FALSE
+                       	EndGlobalSection
+                       EndGlobal
+                       """;
+
+        var actual = solutionFileStringBuilder.Build();
+
+        actual.Should().Be(expected);
+    }
+}
